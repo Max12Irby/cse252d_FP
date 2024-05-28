731d72c396bc00fcd6f65fb1b1ada6f7b123737b
Rami Altai <ramialtai@utexas.edu>
2024-05-27 22:31:03 -0700

    Fix cd directory to go to git repo first

--- script_experimental.sh
+++ script_experimental.sh
@@ -1,1 +1,1 @@
-cd ./detr && python main.py --coco_path /datasets/COCO-2017/ --batch_size 2 --epochs 1 > lastrun.log 2>&1
+cd cse252d_FP/detr && python main.py --coco_path /datasets/COCO-2017/ --batch_size 2 --epochs 1 > lastrun.log 2>&1
