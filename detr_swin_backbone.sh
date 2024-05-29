6d194322bb824befe16cb4d008906a9e3a2d21fd
Rami Altai <ramialtai@utexas.edu>
2024-05-29 01:03:26 -0700

    Put logs in different locations for our two architecture variants

--- detr_swin_backbone.sh
+++ detr_swin_backbone.sh
@@ -1,1 +1,1 @@
-cd cse252d_FP/detr && python main.py --coco_path /datasets/COCO-2017/ --batch_size 2 --epochs 1 > lastrun-backbone.log 2>&1
+cd cse252d_FP/detr && python main.py --coco_path /datasets/COCO-2017/ --batch_size 2 --epochs 1 --output_dir ./output_backbone > lastrun-backbone.log 2>&1
